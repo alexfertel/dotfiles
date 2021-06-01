6af441734eb290ed989d2973b4e916262293fe71
Alexander Gonzalez <alexfertel97@gmail.com>
2021-06-01 14:16:26 -0400

    Add an install script

---
+++ .dotfiles-install.sh
@@ -0,0 +1,14 @@
+git clone --bare https://github.com/alexfertel/dotfiles.git $HOME/.dotfiles
+function dotfiles {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .dotfiles-backup
+dotfiles checkout
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles.";
+  else
+    echo "Backing up pre-existing dot files.";
+    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+dotfiles checkout
+dotfiles config status.showUntrackedFiles no
