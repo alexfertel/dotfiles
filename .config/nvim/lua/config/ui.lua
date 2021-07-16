f0b29c63e5de4f692442fd8f617f401bf90da8b1
Alexander Gonzalez <alexfertel97@gmail.com>
2021-07-16 01:44:31 -0400

    Change to ayu

--- .config/nvim/lua/config/ui.lua
+++ .config/nvim/lua/config/ui.lua
@@ -7,7 +7,6 @@ g.sonokai_diagnostic_line_highlight = 1
 g.sonokai_diagnostic_virtual_text = "colored"
 g.sonokai_better_performance = 1
 
--- g.lightline = {'colorscheme' : 'horizon'}
 g.tokyonight_style = "storm"
 g.tokyonight_enable_italic = 1
 g.airline_theme = "tokyonight"
@@ -15,11 +14,8 @@ g.airline_theme = "material"
 g.airline_theme = "sonokai"
 
 g.ayucolor = "mirage"
--- let ayucolor = "mirage"
--- lua vim.g.ayu_mirage = true
 
--- colorscheme horizon
--- colorscheme tokyonight
-vim.cmd("colorscheme sonokai")
--- colorscheme material
--- colorscheme ayu
+vim.cmd("colorscheme ayu")
+-- vim.cmd("colorscheme sonokai")
+-- vim.cmd("colorscheme tokyonight")
+-- vim.cmd("colorscheme material")
